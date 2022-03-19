d5c49a8d07f9d86cf28b3fc542d3a8548eaaae9f
markl72 <non_111111@hotmail.com>
2022-03-19 11:50:14 +1100

    first commit

---
+++ checkaccount.php
@@ -0,0 +1,51 @@
+<html>
+<body>
+
+<?php
+
+$dbserver = "localhost";
+$dbuserid = "insecureapp";
+$dbpassword = "45EUlZOpL7";
+$db = "insecureapp";
+
+$userid = $_POST["userid"] ?? '';
+$password = $_POST["password"] ?? '';
+
+$conn = new mysqli($dbserver, $dbuserid, $dbpassword, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+// print account details
+$sql = "SELECT * FROM users WHERE userid = '" . $userid . "' AND password = '" . $password . "'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+	// print heading
+	echo "<H1>Your Account</H1>";
+	echo "<p>Welcome, your account details are:</p>";
+
+	while($row = $result->fetch_assoc()) {
+        echo " <p>Name: " . $row["lastname"]. ", " . $row["firstname"]. " <BR>Address: " . $row["address"]. "<br>Phone number: " . $row["phone"] . "</p>";
+    }
+}
+else
+{
+echo "<p><b>Login failed</b></p>";
+}
+
+$conn->close();
+
+
+echo "<p><BR><font color=\"red\">" . $sql . "</font></p>";
+
+
+//https://www.w3schools.com/php/php_mysql_select.asp
+
+?>
+
+</body>
+</html>
---
+++ checkaccount2.php
@@ -0,0 +1,58 @@
+<html>
+<body>
+
+<?php
+
+$userid = $_POST["userid"];
+$password = $_POST["password"];
+
+$dbserver = "localhost";
+$dbuserid = "insecureapp";
+$dbpassword = "45EUlZOpL7";
+$db = "insecureapp";
+
+$conn = new mysqli($dbserver, $dbuserid, $dbpassword, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+// print account details
+
+$sql = "SELECT * FROM users WHERE userid=? AND password=?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $userid, $password);
+$stmt->execute();
+
+$result = $stmt->get_result();
+
+if ($result->num_rows > 0) {
+
+	// print heading
+	echo "<H1>Your Account</H1>";
+	echo "<p>Welcome, your account details are:</p>";
+
+	while($row = $result->fetch_assoc()) {
+        echo " <p>Name: " . htmlentities($row["lastname"]). ", " . htmlentities($row["firstname"]). " <BR>Address: " . htmlentities($row["address"]). "<br>Phone number: " . htmlentities($row["phone"]) . "</p>";
+    }
+}
+else
+{
+echo "<p><b>Login failed</b></p>";
+}
+
+
+$conn->close();
+
+
+echo "<p><BR><font color=\"red\">" . $sql . "</font></p>";
+
+
+//https://websitebeaver.com/prepared-statements-in-php-mysqli-to-prevent-sql-injection
+
+?>
+
+</body>
+</html>
